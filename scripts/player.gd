f12881f6f51b464de2965c21eca7906912ab5780
lukaiakobidze <lukaiakobidze@gmail.com>
2025-02-06 15:24:22 +0400

    player looks the way youre moving

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,11 @@ const JUMP_VELOCITY = -300.0
 const reach = 5
 
 func _physics_process(delta: float) -> void:
+	
+	
+	
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -17,6 +22,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = true
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
